93fe7c82b7781f1be48ce9fbdd14835014db486a
seanremy <seanremy@gmail.com>
2018-02-01 15:23:56 -0500

    fixed testing

--- app/golang/sat_test.go
+++ app/golang/sat_test.go
@@ -1,12 +1,92 @@
 package main
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"os"
+	"path"
+	"path/filepath"
 	"strings"
 	"testing"
 )
+
+func TestInit(t *testing.T) {
+	expectedDataDir, err := filepath.Abs("../../data")
+	if err != nil {
+		t.Fatal(err)
+	}
+	Init(ioutil.Discard, os.Stdout, os.Stdout, os.Stderr)
+
+	if port == nil {
+		t.Errorf("got no port")
+	} else if *dataDir != expectedDataDir {
+		t.Errorf("got %#v, wanted %#v", *dataDir, expectedDataDir)
+	} else {
+		print("Passed TestInit!\n")
+	}
+}
+
+func TestGetPaths(t *testing.T) {
+	a := &Task{}
+	str := `{"AssignmentID": "0000", "ProjectName": "testPath",
+			"StartTime": 1517447292, "SubmitTime": 1517447292}`
+	err := json.Unmarshal([]byte(str), a)
+
+	expectedAssign := path.Join(*dataDir, "Assignments", a.ProjectName,
+		a.AssignmentID+".json")
+	expectedSubmis := path.Join(*dataDir, "Submissions", a.ProjectName,
+		a.AssignmentID, formatTime(a.StartTime)+".json")
+	expectedLatest := path.Join(*dataDir, "Submissions", a.ProjectName,
+		a.AssignmentID, "latest.json")
+	expectedLog := path.Join(GetProjPath(), "Log", a.ProjectName, a.AssignmentID,
+		formatTime(a.SubmitTime)+".json")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if path := a.GetAssignmentPath(); path != expectedAssign {
+		t.Errorf("got %#v, wanted %#v", path, expectedAssign)
+	} else {
+		print("Passed TestGetAssignmentPath!\n")
+	}
+
+	if path := a.GetSubmissionPath(); path != expectedSubmis {
+		t.Errorf("got %#v, wanted %#v", path, expectedSubmis)
+	} else {
+		print("Passed TestGetSubmissionPath!\n")
+	}
+
+	if path := a.GetLatestSubmissionPath(); path != expectedLatest {
+		t.Errorf("got %#v, wanted %#v", path, expectedLatest)
+	} else {
+		print("Passed TestGetLatestSubmissionPath!\n")
+	}
+
+	if path := a.GetLogPath(); path != expectedLog {
+		t.Errorf("got %#v, wanted %#v", path, expectedLog)
+	} else {
+		print("Passed TestGetLogPath!\n")
+	}
+}
+
+func TestMin(t *testing.T) {
+	var a, b int = 1, 2
+	min := Min(a, b)
+	if min != a {
+		t.Errorf("Min was incorrect, got: %d, want: %d.", min, a)
+	} else {
+		print("Passed TestMin!\n")
+	}
+}
+
+func TestGetResult(t *testing.T) {
+}
+
+func TestGetFullResult(t *testing.T) {
+}
 
 func TestIndexHandler(t *testing.T) {
 	// create a Request to pass to the handler
