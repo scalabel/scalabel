11196257fe36823f5ef1cfd0b57a28a35b525538
Fisher Yu <i@yf.io>
2018-02-25 16:01:36 -0800

    change color palette

--- app/js/box.js
+++ app/js/box.js
@@ -801,28 +801,8 @@
         };
 
         BBox.prototype.colors = function(id) {
-            let tableauColors = [
-                'rgb(31, 119, 180)',
-                'rgb(174, 199, 232)',
-                'rgb(255, 127, 14)',
-                'rgb(255, 187, 120)',
-                'rgb(44, 160, 44)',
-                'rgb(152, 223, 138)',
-                'rgb(214, 39, 40)',
-                'rgb(255, 152, 150)',
-                'rgb(148, 103, 189)',
-                'rgb(197, 176, 213)',
-                'rgb(140, 86, 75)',
-                'rgb(196, 156, 148)',
-                'rgb(227, 119, 194)',
-                'rgb(247, 182, 210)',
-                'rgb(127, 127, 127)',
-                'rgb(199, 199, 199)',
-                'rgb(188, 189, 34)',
-                'rgb(219, 219, 141)',
-                'rgb(23, 190, 207)',
-                'rgb(158, 218, 229)'];
-            return tableauColors[id % 20];
+            let c = pickColorPalette(id);
+            return sprintf('rgb(%d %d %d %d)', c[0], c[1], c[2]);
         };
 
         BBox.prototype.hidden_colors = function(id, handleIndex) {
