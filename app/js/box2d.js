62c47628d1cc1477f86e9cc2a5f126d800552435
seanremy <seanremy@gmail.com>
2018-03-06 00:00:10 -0500

    filling out box2d draw methods

--- app/js/box2d.js
+++ app/js/box2d.js
@@ -26,3 +26,97 @@ function Box2d(sat, id) {
 }
 
 Box2d.prototype = Object.create(ImageLabel.prototype);
+
+/**
+ * Draw this bounding box on the canvas.
+ * @param {object} canvas - HTML canvas for visible objects.
+ * @param {object} hiddenCanvas - HTML canvas for hidden objects.
+ * @param {number} selectedBox - ID of the currently selected box, or null if
+ *   no box selected.
+ * @param {boolean} resizing - Whether or not this box is being resized.
+ */
+Box2d.prototype.redraw = function(canvas, hiddenCanvas, selectedBox, resizing) {
+  let self = this;
+
+  // get contexts from canvases
+  let ctx = canvas.getContext('2d');
+  let hiddenCtx = hiddenCanvas.getContext('2d');
+
+  // draw visible elements
+  self.drawBox(ctx, selectedBox, resizing);
+
+  // draw hidden elements
+  self.drawHiddenBox(hiddenCtx, selectedBox)
+
+}
+
+/**
+ * Draw the box part of this bounding box.
+ * @param {object} canvas - HTML canvas for visible objects.
+ * @param {object} ctx - Canvas context.
+ * @param {number} selectedBox - ID of the currently selected box, or null if
+ *   no box selected.
+ * @param {boolean} resizing - Whether or not this box is being resized.
+ */
+Box2d.prototype.drawBox = function(ctx, selectedBox, resizing) {
+  ctx.save(); // save the canvas context settings
+  if (selectedBox && selectedBox != this.id) {
+    // if exists selected box and it's not this one, alpha this out
+    ctx.globalAlpha = FADED_ALPHA; // TODO: where is FADED_ALPHA?
+  }
+  if (resizing) {
+    ctx.setLineDash([3]); // if box is being resized, use line dashes
+  }
+  if (this.isSmall()) {
+    ctx.strokeStyle = 'rgb(169, 169, 169)' // if box is too small, gray it out
+  } else {
+    // otherwise use regular color
+    ctx.strokeStyle = this.color();
+  }
+  ctx.lineWidth = LINE_WIDTH; // set line width TODO: where is LINE_WIDTH?
+  ctx.strokeRect(this.x1, this.y1, this.w, this.h); // draw the box
+  ctx.restore(); // restore the canvas to saved settings
+}
+
+Box2d.prototype.drawHandles = function() {
+
+}
+
+Box2d.prototype.drawHandleOutlines = function() {
+
+}
+
+Box2d.prototype.drawLabel = function() {
+
+}
+
+/**
+ * Draw the box part of the hidden box.
+ * @param {object} hiddenCtx - Hidden canvas context.
+ * @param {number} selectedBox - ID of the currently selected box, or null if
+ *   no box selected.
+ */
+Box2d.prototype.drawHiddenBox = function(hiddenCtx, selectedBox) {
+  // only draw if it is not the case that there is another selected box
+  if (!selectedBox || selectedBox === this.id) {
+    hiddenCtx.save(); // save the canvas context settings
+    hiddenCtx.strokeStyle = this.hiddenColor(8); // 8 represents the box itself
+    hiddenCtx.lineWidth = HIDDEN_LINE_WIDTH; // TODO: where is HIDDEN_LINE_WIDTH?
+    hiddenCtx.strokeRect(this.x1, this.y1, this.w, this.h); // draw the box
+    hiddenCtx.restore(); // restore the canvas to saved settings
+  }
+}
+
+/**
+ * Get whether this bounding box is too small.
+ * @return {boolean} - True if the box is too small.
+ */
+Box2d.prototype.isSmall = function() {
+  return Math.min(this.w, this.h) < MIN_BOX_SIZE;
+  // TODO: where do final vars like MIN_BOX_SIZE go?
+  // TODO: define Box2d variables
+}
+
+Box2d.prototype.hiddenColor = function(handle) {
+
+}
