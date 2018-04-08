375c54e4f3cde703b26289b81056cf243b528a37
bhpfelix <bhpfelix@gmail.com>
2018-04-08 12:50:57 -0400

    fixe link issue in monitor

--- app/golang/utils.go
+++ app/golang/utils.go
@@ -56,6 +56,7 @@ func GetTask(projName string, taskName string) Task {
 		projName,
 		taskName+".json",
 	)
+
 	// TODO: error handling
 	fileContents, _ := ioutil.ReadFile(taskPath)
 	task := Task{}
