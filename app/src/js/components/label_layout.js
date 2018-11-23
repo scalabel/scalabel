c2c4fafa257b59f1bc7ebeb9f94920eaa1272110
Fisher Yu <i@yf.io>
2018-11-23 01:31:24 -0800

    fix material-ui flow type install

--- app/src/js/components/label_layout.js
+++ app/src/js/components/label_layout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import CssBaseline from '@material-ui/core';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import SplitPane from 'react-split-pane';
 
 type Props = {
