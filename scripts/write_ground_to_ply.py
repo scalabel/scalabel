afcd9be30cdd11019acf68a892362791a79fb5af
gycn <garychen@berkeley.edu>
2019-08-09 23:54:03 -0700

    Lint write_ground_to_ply.py (#180)
    * Lint write_ground_to_ply.py
    
    * change line length
    
    * no long line exception
    
    * more linting
    
    * more linting
    
    * remove else after return
    
    * Use zip instead for range(len(...
    
    * fix write_ground_to_ply.py

--- scripts/write_ground_to_ply.py
+++ scripts/write_ground_to_ply.py
@@ -1,116 +1,141 @@
+'''
+Script for detecting ground in point clouds and writing it back to the PLY file
+'''
+
 import argparse
-import numpy as np
 import os
 import re
 import sys
 
+import numpy as np
+
 
 def find_ground_plane(points, normals, iters,
-                      minAcceptedSize,
-                      minDist, maxDist, minHeight, maxHeight,
-                      expectedNormal):
+                      min_accepted_size,
+                      min_dist, max_dist, min_height, max_height,
+                      expected_normal):
+    '''
+    Find ground plane
+    :param points: list of points
+    :param normals: normal vectors
+    :param iters: number of times to run ransac
+    :param min_accepted_size: smallest plane size (number of points)
+    :param min_dist: min dist of points from origin to be considered in ransac
+    :param max_dist: max dist ""
+    :param min_height: min height ""
+    :param max_height: max height ""
+    :param expected_normal: Expected normal direction of the plane
+    :return:
+    '''
     depths = np.linalg.norm(points, axis=-1)
-    heightFiltered = np.logical_and(points[:, 2] >= minHeight,
-                                    points[:, 2] <= maxHeight)
-    distFiltered = np.logical_and(depths <= maxDist, depths >= minDist)
-    closeToExpected = np.abs(normals.dot(expectedNormal)) >= 0.9
-    validIndices = np.logical_and(np.logical_and(heightFiltered, distFiltered),
-                                  closeToExpected)
-    validPoints = points[validIndices]
-    validNormals = normals[validIndices]
+    height_filtered = np.logical_and(points[:, 2] >= min_height,
+                                     points[:, 2] <= max_height)
+    dist_filtered = np.logical_and(depths <= max_dist, depths >= min_dist)
+    close_to_expected = np.abs(normals.dot(expected_normal)) >= 0.9
+    valid_indices = np.logical_and(
+        np.logical_and(height_filtered, dist_filtered),
+        close_to_expected)
+    valid_points = points[valid_indices]
+    valid_normals = normals[valid_indices]
 
-    maxNumInliers = 0
-    bestPlane = None
-    bestPlaneInliers = None
-    bestPlaneOutliers = None
+    max_num_inliers = 0
+    best_plane = None
+    best_plane_inliers = None
+    best_plane_outliers = None
 
-    for i in range(iters):
-        sampleIndex = np.random.randint(validPoints.shape[0])
-        samplePoint = validPoints[sampleIndex]
-        sampleNormal = validNormals[sampleIndex]
+    for _ in range(iters):
+        sample_index = np.random.randint(valid_points.shape[0])
+        sample_point = valid_points[sample_index]
+        sample_normal = valid_normals[sample_index]
 
-        normalCosAngles = validNormals.dot(sampleNormal)
+        normal_cos_angles = valid_normals.dot(sample_normal)
 
-        sameDirIndices = normalCosAngles >= 0.8
-        sameDirPoints = validPoints[sameDirIndices]
+        same_dir_indices = normal_cos_angles >= 0.8
+        same_dir_points = valid_points[same_dir_indices]
 
-        diffs = sameDirPoints - samplePoint
+        diffs = same_dir_points - sample_point
         dists = np.linalg.norm(diffs, axis=-1)
         diffs[:, 0] /= dists
         diffs[:, 1] /= dists
         diffs[:, 2] /= dists
 
-        distsToPlane = (diffs).dot(sampleNormal)
+        dists_to_plane = (diffs).dot(sample_normal)
 
-        samePlaneIndices = np.abs(distsToPlane) <= 0.15
+        same_plane_indices = np.abs(dists_to_plane) <= 0.15
 
-        samePlanePoints = sameDirPoints[samePlaneIndices]
+        same_plane_points = same_dir_points[same_plane_indices]
 
-        if samePlanePoints.shape[0] > maxNumInliers:
-            cov = np.cov(samePlanePoints, rowvar=False)
+        if same_plane_points.shape[0] > max_num_inliers:
+            cov = np.cov(same_plane_points, rowvar=False)
             eigvals, eigvecs = np.linalg.eigh(cov)
             normal = eigvecs[:, np.argmin(eigvals)]
-            center = np.average(samePlanePoints, axis=0)
+            center = np.average(same_plane_points, axis=0)
             offset = -normal.dot(center)
 
-            outliers1 = validPoints[np.logical_not(sameDirIndices)]
-            outliers2 = sameDirPoints[np.logical_not(samePlaneIndices)]
-            if samePlanePoints.shape[0] > validPoints.shape[0] * \
-               minAcceptedSize:
-                return normal, offset, samePlanePoints, np.vstack((outliers1,
-                                                                   outliers2))
-            maxNumInliers = samePlanePoints.shape[0]
-            bestPlane = (normal, offset)
-            bestPlaneInliers = samePlanePoints
-            bestPlaneOutliers = np.vstack((outliers1, outliers2))
+            outliers1 = valid_points[np.logical_not(same_dir_indices)]
+            outliers2 = same_dir_points[np.logical_not(same_plane_indices)]
+            if same_plane_points.shape[0] > valid_points.shape[0] * \
+                    min_accepted_size:
+                return normal, offset, same_plane_points, np.vstack(
+                    (outliers1, outliers2))
+            max_num_inliers = same_plane_points.shape[0]
+            best_plane = (normal, offset)
+            best_plane_inliers = same_plane_points
+            best_plane_outliers = np.vstack((outliers1, outliers2))
 
-    return bestPlane[0], bestPlane[1], bestPlaneInliers, bestPlaneOutliers
+    return best_plane[0], best_plane[1], \
+        best_plane_inliers, best_plane_outliers
 
 
-def loadPlyFiles(directoryName):
-    if os.path.isdir(directoryName):
-        filenames = os.listdir(directoryName)
+def load_ply_files(directory_name):
+    '''
+    Load PLY files in directory, must have both points & normals
+    :param directory_name: name of directory
+    :return: array of all point clouds, array of normals, filenames
+    '''
+    if os.path.isdir(directory_name):
+        filenames = os.listdir(directory_name)
 
-        pointsArr = []
-        normalsArr = []
-        outputFileNames = []
+        points_arr = []
+        normals_arr = []
+        output_file_names = []
         for filename in sorted(filenames):
             if filename.split('.')[-1] != 'ply':
                 continue
 
-            outputFileNames.append(directoryName + '/' + filename)
+            output_file_names.append(directory_name + '/' + filename)
 
-            contents = open(outputFileNames[-1], 'rb').read()
-            endHeaderString = 'end_header\n'.encode('utf-8')
-            endHeaderPosition = contents.find(endHeaderString)
-            if endHeaderPosition == -1:
+            contents = open(output_file_names[-1], 'rb').read()
+            end_header_string = 'end_header\n'.encode('utf-8')
+            end_header_position = contents.find(end_header_string)
+            if end_header_position == -1:
                 print('Invalid ply header: {}'.format(filename))
                 continue
 
-            dataStartInd = endHeaderPosition + len(endHeaderString)
+            data_start_ind = end_header_position + len(end_header_string)
 
-            header = contents[:dataStartInd].decode('utf-8')
-            headerLines = header.split('\n')
-            if headerLines[0] != 'ply':
+            header = contents[:data_start_ind].decode('utf-8')
+            header_lines = header.split('\n')
+            if header_lines[0] != 'ply':
                 print('Invalid ply header: {}'.format(filename))
                 continue
 
-            formatString = headerLines[1].split()[1]
+            format_string = header_lines[1].split()[1]
 
-            elementProg = re.compile('element')
-            propertyProg = re.compile('property')
+            element_prog = re.compile('element')
+            property_prog = re.compile('property')
             properties = []
-            numVertices = -1
+            num_vertices = -1
             error = False
-            for i in range(2, len(headerLines)):
-                if elementProg.match(headerLines[i]):
-                    if numVertices != -1:
+            for i in range(2, len(header_lines)):
+                if element_prog.match(header_lines[i]):
+                    if num_vertices != -1:
                         print('Multiple element declarations: {}'.format(
                             filename))
                         error = True
                         break
 
-                    tokens = headerLines[i].split()
+                    tokens = header_lines[i].split()
                     if len(tokens) != 3:
                         print('Invalid element declaration (Line {}): '
                               '{}'.format(i, filename))
@@ -123,15 +148,15 @@ def loadPlyFiles(directoryName):
                         error = True
                         break
 
-                    numVertices = int(tokens[2])
-                elif propertyProg.match(headerLines[i]):
-                    if numVertices == -1:
+                    num_vertices = int(tokens[2])
+                elif property_prog.match(header_lines[i]):
+                    if num_vertices == -1:
                         print('Property declaration without element '
                               'declaration: {}'.format(filename))
                         error = True
                         break
 
-                    tokens = headerLines[i].split()
+                    tokens = header_lines[i].split()
                     if len(tokens) != 3:
                         print('Invalid property declaration: '
                               '{}'.format(filename))
@@ -159,40 +184,39 @@ def loadPlyFiles(directoryName):
                 print('Missing vertex properties: {}'.format(filename))
                 continue
 
-            rawData = contents[dataStartInd:]
+            raw_data = contents[data_start_ind:]
 
-            if formatString == 'ascii':
-                dataTokens = rawData.split()
-                if len(dataTokens) != numVertices * 6:
+            if format_string == 'ascii':
+                data_tokens = raw_data.split()
+                if len(data_tokens) != num_vertices * 6:
                     print('Number of data points is different from what is '
                           'specified: {}'.format(filename))
                     continue
-                data = [float(token) for token in dataTokens]
-                data = np.array(data).reshape(numVertices, 6)
-            elif formatString == 'binary_little_endian':
-                data = np.fromstring(rawData, dtype='<f4').reshape(
-                    (numVertices, 6))
-            elif formatString == 'binary_big_endian':
-                data = np.fromstring(rawData, dtype='>f4').reshape(
-                    (numVertices, 6))
+                data = [float(token) for token in data_tokens]
+                data = np.array(data).reshape(num_vertices, 6)
+            elif format_string == 'binary_little_endian':
+                data = np.fromstring(raw_data, dtype='<f4').reshape(
+                    (num_vertices, 6))
+            elif format_string == 'binary_big_endian':
+                data = np.fromstring(raw_data, dtype='>f4').reshape(
+                    (num_vertices, 6))
 
-            points = np.zeros((numVertices, 3))
-            normals = np.zeros((numVertices, 3))
+            points = np.zeros((num_vertices, 3))
+            normals = np.zeros((num_vertices, 3))
 
             error = False
-            for i in range(len(properties)):
-                property = properties[i]
-                if property == 'x':
+            for i, prpty in enumerate(properties):
+                if prpty == 'x':
                     points[:, 0] = data[:, i]
-                elif property == 'y':
+                elif prpty == 'y':
                     points[:, 1] = data[:, i]
-                elif property == 'z':
+                elif prpty == 'z':
                     points[:, 2] = data[:, i]
-                elif property == 'nx':
+                elif prpty == 'nx':
                     normals[:, 0] = data[:, i]
-                elif property == 'ny':
+                elif prpty == 'ny':
                     normals[:, 1] = data[:, i]
-                elif property == 'nz':
+                elif prpty == 'nz':
                     normals[:, 2] = data[:, i]
                 else:
                     print('Invalid property declaration: {}'.format(filename))
@@ -201,16 +225,18 @@ def loadPlyFiles(directoryName):
             if error:
                 continue
 
-            pointsArr.append(points.astype(np.float32))
-            normalsArr.append(normals.astype(np.float32))
+            points_arr.append(points.astype(np.float32))
+            normals_arr.append(normals.astype(np.float32))
 
-        return pointsArr, normalsArr, outputFileNames
-    else:
-        print("Invalid directory: {}".format(directoryName))
-        return None, None, None
+        return points_arr, normals_arr, output_file_names
+    print("Invalid directory: {}".format(directory_name))
+    return None, None, None
 
 
-if __name__ == '__main__':
+def main():
+    '''
+    Main function
+    '''
     parser = argparse.ArgumentParser(
         description='Find ground plane and write it to PLY file')
     parser.add_argument('--pointclouddir',
@@ -219,50 +245,56 @@ if __name__ == '__main__':
     parser.add_argument('--iterations',
                         help='Number of iterations to run for RANSAC.',
                         default=20, type=int)
-    parser.add_argument('--minAcceptedFraction',
+    parser.add_argument('--min_accepted_fraction',
                         help='Minimum ratio of points in found ground plane '
                              'to total valid points for plane to be '
                              'automatically accepted',
                         default=0.4, type=float)
-    parser.add_argument('--minDist', help='Minimum distance of points from '
-                                          'origin to be considered valid',
+    parser.add_argument('--min_dist', help='Minimum distance of points from '
+                                           'origin to be considered valid',
                         default=3, type=float)
-    parser.add_argument('--maxDist', help='Maximum distance of points from '
-                                          'origin to be considered valid',
+    parser.add_argument('--max_dist', help='Maximum distance of points from '
+                                           'origin to be considered valid',
                         default=12, type=float)
-    parser.add_argument('--minHeight', help='Minimum height of points, value '
-                                            'on z-axis to be considered valid',
+    parser.add_argument('--min_height',
+                        help='Minimum height of points, value '
+                             'on z-axis to be considered valid',
                         default=-2, type=float)
-    parser.add_argument('--maxHeight', help='Maximum height of points, value '
-                                            'on z-axis to be considered valid',
+    parser.add_argument('--max_height',
+                        help='Maximum height of points, value '
+                        'on z-axis to be considered valid',
                         default=-1, type=float)
-    parser.add_argument('--expectedNormal',
+    parser.add_argument('--expected_normal',
                         help='Expected normal of the ground plane',
                         nargs='+', default=[0, 0, 1], type=float)
 
     args = parser.parse_args()
 
-    if len(args.expectedNormal) != 3:
+    if len(args.expected_normal) != 3:
         print('Expected normal must be a 3 element array.')
         sys.exit(0)
 
-    pointsArr, normalsArr, filenames = loadPlyFiles(args.pointclouddir)
+    loaded_points, loaded_normals, file_names = \
+        load_ply_files(args.pointclouddir)
 
-    if pointsArr is not None:
-        for i in range(len(pointsArr)):
-            groundNormal, groundOffset, _, _ = \
-                find_ground_plane(pointsArr[i], normalsArr[i], args.iterations,
-                                  args.minAcceptedFraction,
-                                  args.minDist, args.maxDist,
-                                  args.minHeight, args.maxHeight,
-                                  np.array(args.expectedNormal))
-            f = open(filenames[i], 'w')
+    if loaded_points is not None:
+        for points, normals, file_name in zip(
+                loaded_points, loaded_normals, file_names
+        ):
+            ground_normal, ground_offset, _, _ = \
+                find_ground_plane(points, normals,
+                                  args.iterations,
+                                  args.min_accepted_fraction,
+                                  args.min_dist, args.max_dist,
+                                  args.min_height, args.max_height,
+                                  np.array(args.expected_normal))
+            f = open(file_name, 'w')
             f.write("ply\n")
             f.write("format binary_little_endian 1.0\n")
             f.write("comment [groundCoefficients] {}, {}, {}, {}\n".format(
-                groundNormal[0], groundNormal[1], groundNormal[2],
-                groundOffset))
-            f.write("element vertex {}\n".format(pointsArr[i].shape[0]))
+                ground_normal[0], ground_normal[1], ground_normal[2],
+                ground_offset))
+            f.write("element vertex {}\n".format(points.shape[0]))
             f.write("property float x\n")
             f.write("property float y\n")
             f.write("property float z\n")
@@ -272,10 +304,14 @@ if __name__ == '__main__':
             f.write("end_header\n")
 
             f.close()
-            f = open(filenames[i], 'ab')
+            f = open(file_name, 'ab')
 
-            for j in range(pointsArr[i].shape[0]):
-                f.write(pointsArr[i][j].tobytes())
-                f.write(normalsArr[i][j].tobytes())
+            for i in range(points.shape[0]):
+                f.write(points[i].tobytes())
+                f.write(normals[i].tobytes())
 
             f.close()
+
+
+if __name__ == '__main__':
+    main()
