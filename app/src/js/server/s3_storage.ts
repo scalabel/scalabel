38edbb14b0677de4ae78c33059f4b63c099f8b18
Chris Powers <chris.powers@berkeley.edu>
2020-01-13 17:51:46 -0800

    start s3

--- app/src/js/server/s3_storage.ts
+++ app/src/js/server/s3_storage.ts
@@ -1,18 +1,74 @@
-import * as fs from 'fs-extra'
+import * as aws from 'aws-sdk'
 import * as path from 'path'
 import { Storage } from './storage'
 
 /**
  * Implements local file storage
  */
-export class FileStorage extends Storage {
+export class S3Storage extends Storage {
+  /** the region name */
+  protected region: string
+  /** the bucket name */
+  protected bucketName: string
+  /** the path within the bucket */
+  protected dataPath: string
+  /** the aws s3 client */
+  protected s3: aws.s3
+
   /**
    * Constructor
    */
-  constructor (dataDir: string) {
+  async constructor (dataDir: string): Promise<void> {
     super(dataDir)
-    // do this synchronously (only once)
-    fs.ensureDirSync(this.dataDir)
+
+    // data path should have format region:bucket/path
+    info = dataDir.split(':')
+    this.region = info[0]
+    bucketPath = info[1].split('/')
+    this.bucketName = bucketPath[0]
+    this.dataPath = path.join(bucketPath[1:], '/')
+    this.s3 = new aws.s3()
+
+    //create new bucket if there isn't one already (wait until it exists)
+    const hasBucket = await this.hasBucket()
+    if (!hasBucket) {
+      const bucketParams = {
+        Bucket: this.bucketName,
+        CreateBucketConfiguration: {
+          LocationConstraint: this.region,
+        }
+      }
+
+      this.createBucket.then()
+      const createBucketPromise = this.s3.createBucket(bucketParams).promise()
+      const checkBucketPromise = this.s3.waitFor('bucketExists', bucketParams).promise()
+
+      createBucketPromise.then((_data: string) => {
+        checkBucketPromise
+      })
+
+      const bucketCreation = new Promise ((resolve, reject) => {
+        this.s3.createBucket(bucketParams, (error: Error, data: string) => {
+        if (error) {
+          reject(error)
+          return
+        }
+        resolve()
+      })
+
+      const bucketCreated = new Promise ((resolve, reject) => {
+        this.s3.waitFor('bucketExists', params, (err: Error, data: string) => {
+          if (err) {
+            reject(error)
+            return
+          }
+          resolve()
+        })
+      })
+
+    } else {
+      return Promise.resolve()
+    }
   }
 
   /**
@@ -94,16 +150,21 @@ export class FileStorage extends Storage {
   }
 
   /**
-   * Makes relative path into full path
+   * Checks if bucket exists
    */
-  private fullDir (key: string): string {
-    return path.join(this.dataDir, key)
-  }
+  private async hasBucket (): Promise<bool> {
+    const params = {
+      Bucket: this.bucketName
+    }
 
-  /**
-   * Makes relative path into full filename
-   */
-  private fullFile (key: string): string {
-    return this.fullDir(key + '.json')
-  }
+    return new Promise((resolve, reject) => {
+      this.s3.HeadBucket(params, (error: Error, data: string) => {
+        if (error) {
+          resolve(false)
+        } else {
+          resolve(true)
+        }
+      })
+    })
+      }
 }
