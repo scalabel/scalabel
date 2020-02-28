15a106bcf7810f6179b2f78602931eee2b2b3b64
Chris Powers <chris.powers@berkeley.edu>
2020-02-28 10:48:14 -0800

    document plan

--- app/src/js/server/session_manager.ts
+++ app/src/js/server/session_manager.ts
@@ -9,12 +9,18 @@ export class SessionManager {
 
   constructor (config: ServerConfig) {
     this.config = config
+    // store a map from task id to virtual session
   }
 
   /**
    * Listens to redis changes
    */
   public listen () {
+    // when a new task is added to redis, create a virtual session
+    // virtual session should use socketio channels
+
+    // use redis keyevents to subscribe to set events
+    // make sure its a saveDir, then extract the taskId
     return
   }
 }
