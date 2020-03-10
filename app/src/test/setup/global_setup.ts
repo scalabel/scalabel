aa971f137be26cd46eac55fbf2a59fe6fbc49f87
Fisher Yu <i@yf.io>
2020-03-10 12:09:39 -0700

    Mute logger info for unit test to show test stats clearly

--- app/src/test/setup/global_setup.ts
+++ app/src/test/setup/global_setup.ts
@@ -1,10 +1,12 @@
 import * as child from 'child_process'
+import Logger from '../../js/server/logger'
 
 module.exports = async () => {
+  Logger.mute(true)
   // make sure this matches the redis port in test_config.yml
   const redisProc = child.spawn('redis-server', ['--appendonly', 'no',
     '--save', '', '--port', '6378', '--bind', '127.0.0.1',
-    '--protected-mode', 'yes'])
+    '--protected-mode', 'yes', '--loglevel', 'warning'])
   redisProc.stdout.on('data', (data) => {
     process.stdout.write(data)
   })
