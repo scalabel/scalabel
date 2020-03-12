b577b7e1cb4c581b35cac50cc422aec3a1d9db90
Xinyin Li <xli1@xiaohongshu.com>
2020-03-13 00:59:59 +0800

    Add test

--- app/src/test/components/toolbar.test.tsx
+++ app/src/test/components/toolbar.test.tsx
@@ -7,12 +7,14 @@ import { create } from 'react-test-renderer'
 import * as action from '../../js/action/common'
 import { selectLabel } from '../../js/action/select'
 import Session from '../../js/common/session'
-import { initStore } from '../../js/common/session_init'
+import { initStore, loadImages } from '../../js/common/session_init'
 import { ToolBar } from '../../js/components/toolbar'
 import { Category } from '../../js/components/toolbar_category'
 import { ListButton } from '../../js/components/toolbar_list_button'
+import { Viewer2D } from '../../js/components/viewer2d'
 import { makeLabel } from '../../js/functional/states'
 import { testJson } from '../test_image_objects'
+import { testJson as testTrackJson } from '../test_track_objects'
 
 let handleToggleWasCalled: boolean = false
 const testValues = ['NA', 'A', 'B', 'C']
@@ -160,5 +162,104 @@ describe('test functionality for attributes with multiple values', () => {
     NAButton = getByTestId('toggle-button-NA') as (HTMLButtonElement)
     expect(handleToggleWasCalled).toBe(true)
     expect(NAButton.className).toContain('selected')
+  })
+})
+
+describe('test track', () => {
+  test('Delete by click', () => {
+    Session.devMode = false
+    initStore(testTrackJson)
+    loadImages()
+    const toolbarRef: React.Ref<ToolBar> = React.createRef()
+    const { getByText } = render(
+    <ToolBar
+      ref={toolbarRef}
+      categories={null}
+      attributes={[]}
+      labelType={'labelType'}
+      />
+    )
+    expect(toolbarRef.current).not.toBeNull()
+    expect(toolbarRef.current).not.toBeUndefined()
+    if (toolbarRef.current) {
+      toolbarRef.current.componentDidMount()
+    }
+
+    Session.dispatch(action.goToItem(1))
+    let state = Session.getState()
+    const trackLabels = state.task.tracks[3].labels
+    const lblInItm2 = trackLabels[2]
+    const lblInItm3 = trackLabels[3]
+    const lblInItm4 = trackLabels[4]
+    const lblInItm5 = trackLabels[5]
+    expect(_.size(state.task.tracks[3].labels)).toBe(6)
+    expect(_.size(state.task.items[2].labels)).toBe(3)
+    expect(_.size(state.task.items[2].shapes)).toBe(3)
+    Session.dispatch(selectLabel(
+      Session.getState().user.select.labels, 1, 70))
+    fireEvent(
+      getByText('End Object Tracking'),
+      new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true
+      })
+    )
+    state = Session.getState()
+    expect(_.size(state.task.tracks[3].labels)).toBe(2)
+    expect(state.task.items[2].labels[lblInItm2]).toBeUndefined()
+    expect(state.task.items[2].labels[lblInItm3]).toBeUndefined()
+    expect(state.task.items[2].labels[lblInItm4]).toBeUndefined()
+    expect(state.task.items[2].labels[lblInItm5]).toBeUndefined()
+  })
+  test('Link by click', () => {
+    Session.devMode = false
+    initStore(testTrackJson)
+    loadImages()
+    const toolbarRef: React.Ref<ToolBar> = React.createRef()
+    const { getByText } = render(
+    <ToolBar
+      ref={toolbarRef}
+      categories={null}
+      attributes={[]}
+      labelType={'labelType'}
+      />
+    )
+    expect(toolbarRef.current).not.toBeNull()
+    expect(toolbarRef.current).not.toBeUndefined()
+    if (toolbarRef.current) {
+      toolbarRef.current.componentDidMount()
+    }
+
+    Session.dispatch(action.goToItem(3))
+    let state = Session.getState()
+    expect(_.size(state.task.tracks[2].labels)).toBe(4)
+    expect(_.size(state.task.tracks[9].labels)).toBe(1)
+    expect(state.task.items[5].labels[203].track).toEqual(9)
+
+    Session.dispatch(selectLabel(
+      Session.getState().user.select.labels, 3, 49))
+    fireEvent(
+      getByText('Track-Link'),
+      new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true
+      })
+    )
+
+    Session.dispatch(action.goToItem(5))
+    Session.dispatch(selectLabel(
+      Session.getState().user.select.labels, 5, 203))
+    fireEvent(
+      getByText('Finish Track-Link'),
+      new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true
+      })
+    )
+
+    state = Session.getState()
+    expect(_.size(state.task.tracks[2].labels)).toBe(5)
+    expect(state.task.tracks[9]).toBeUndefined()
+    expect(state.task.items[5].labels[203].track).toEqual(2)
   })
 })
