ab6491b2d122a06794952a2d158f0030621e79f5
Chris Powers <chris.powers@berkeley.edu>
2020-03-27 12:33:20 -0700

    add patch

--- scripts/install_polyrnn.sh
+++ scripts/install_polyrnn.sh
@@ -6,7 +6,7 @@ git clone https://github.com/fidler-lab/polyrnn-pp.git
 # download models separately?
 # apply the patch
 cd polyrnn-pp
-patch -p0 -i ../polyrnn.patch
+patch -p0 -i ../../polyrnn.patch
 cd ../../../..
 # add this to bashrc?
 export PYTHONPATH="${PYTHONPATH}:scalabel/bot/models/polyrnn-pp-pytorch-small/code"
