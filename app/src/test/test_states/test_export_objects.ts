9897ad6588955fbc8e870424038c111d6be4baad
Chris Powers <chris.powers@berkeley.edu>
2020-05-04 11:49:28 -0700

    fix label id export

--- app/src/test/test_states/test_export_objects.ts
+++ app/src/test/test_states/test_export_objects.ts
@@ -9,22 +9,6 @@ export const sampleItemExportImage: ItemExport = {
   sensor: -1,
   labels: [
     {
-      id: '0',
-      category: 'person',
-      attributes: {},
-      manualShape: true,
-      box2d: {
-        x1: 317.9044230030514,
-        y1: 192.46537396121883,
-        x2: 676.9071930861539,
-        y2: 411.85595567867034
-      },
-      poly2d: null,
-      box3d: null,
-      plane3d: null,
-      customs: {}
-    },
-    {
       id: '1',
       category: 'person',
       attributes: {},
@@ -55,6 +39,22 @@ export const sampleItemExportImage: ItemExport = {
       box3d: null,
       plane3d: null,
       customs: {}
+    },
+    {
+      id: 'longId',
+      category: 'person',
+      attributes: {},
+      manualShape: true,
+      box2d: {
+        x1: 317.9044230030514,
+        y1: 192.46537396121883,
+        x2: 676.9071930861539,
+        y2: 411.85595567867034
+      },
+      poly2d: null,
+      box3d: null,
+      plane3d: null,
+      customs: {}
     }
   ]
 }
@@ -69,22 +69,6 @@ export const sampleStateExportImage: ItemExport[] =
       timestamp: 0,
       sensor: -1,
       labels: [
-        {
-          id: '0',
-          category: 'person',
-          attributes: {},
-          manualShape: true,
-          box2d: {
-            x1: 317.9044230030514,
-            y1: 192.46537396121883,
-            x2: 676.9071930861539,
-            y2: 411.85595567867034
-          },
-          poly2d: null,
-          box3d: null,
-          plane3d: null,
-          customs: {}
-        },
         {
           id: '1',
           category: 'person',
@@ -111,6 +95,22 @@ export const sampleStateExportImage: ItemExport[] =
             y1: 137.61772853185596,
             x2: 1018.9570545819988,
             y2: 294.1828254847645
+          },
+          poly2d: null,
+          box3d: null,
+          plane3d: null,
+          customs: {}
+        },
+        {
+          id: 'longId',
+          category: 'person',
+          attributes: {},
+          manualShape: true,
+          box2d: {
+            x1: 317.9044230030514,
+            y1: 192.46537396121883,
+            x2: 676.9071930861539,
+            y2: 411.85595567867034
           },
           poly2d: null,
           box3d: null,
