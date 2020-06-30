fbfa6165aac76f494abf12ff880a0b6990f50fb8
Chris Powers <chris.powers@berkeley.edu>
2020-06-30 16:40:05 -0700

    make makeLabel immutable and add test

--- app/src/test/functional/state.test.ts
+++ app/src/test/functional/state.test.ts
@@ -13,6 +13,7 @@ describe('Defaults get overwritten', () => {
     expect(itemExport.timestamp).toBe(timestamp)
     expect(itemExport.url).toBe('')
   })
+
   test('Label export creation',() => {
     const label = makeLabel()
     const category = 'category'
@@ -23,5 +24,22 @@ describe('Defaults get overwritten', () => {
     expect(labelExport.id).toBe(label.id)
     expect(labelExport.category).toBe(category)
     expect(labelExport.box2d).toBe(null)
+  })
+
+  test('Attributes are immutable', () => {
+    const attributes = {
+      0: [1],
+      1: [0]
+    }
+    const label = makeLabel({
+      attributes
+    })
+    expect(label.attributes[0]).toStrictEqual([1])
+    expect(label.attributes[1]).toStrictEqual([0])
+
+    // Mutating original attribute should not affect the label
+    attributes[0] = [0]
+    expect(label.attributes[0]).toStrictEqual([1])
+    expect(label.attributes[1]).toStrictEqual([0])
   })
 })
