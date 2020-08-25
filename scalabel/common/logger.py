9f2662bbeb1879b51eb9eff0116f36955e8af762
Fisher Yu <fyu@users.noreply.github.com>
2020-08-25 13:18:35 -0700

    Add python tool to create url field in label files (#219)

---
+++ scalabel/common/logger.py
@@ -0,0 +1,8 @@
+"""Define logger and its format."""
+
+import logging
+
+FORMAT = "[%(asctime)-15s %(filename)s:%(lineno)d %(funcName)s] %(message)s"
+logging.basicConfig(format=FORMAT)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
