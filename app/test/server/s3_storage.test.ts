56a0bec2068c184e128151b9fca1cd10dc193274
Fisher Yu <i@yf.io>
2021-03-18 21:29:53 +0100

    Use aws config action

--- app/test/server/s3_storage.test.ts
+++ app/test/server/s3_storage.test.ts
@@ -58,7 +58,7 @@ async function pathExists(key: string): Promise<boolean> {
   }
 }
 
-describe.skip("test s3 storage", () => {
+describe("test s3 storage", () => {
   test("make dir", async () => {
     for (const f of STORAGE_FOLDERS) {
       await storage.mkdir(f)
