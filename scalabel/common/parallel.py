95e2d93629662a55a5246828ca8b44671b281916
Fisher Yu <i@yf.io>
2021-04-22 14:12:15 +0200

    func arg can be None

--- scalabel/common/parallel.py
+++ scalabel/common/parallel.py
@@ -20,7 +20,7 @@ Return = TypeVar("Return")
 # Need variadic input type here
 def run(
     func: Callable[[Inputs], Return],
-    q_in: "Queue[Tuple[int, Optional[Inputs]]]",
+    q_in: "Queue[Tuple[int, Optional[Tuple[Inputs]]]]",
     q_out: "Queue[Tuple[int, Return]]",
 ) -> None:
     """Run function on the inputs from the queue."""
@@ -28,7 +28,7 @@ def run(
         i, x = q_in.get()
         if i < 0 or x is None:
             break
-        q_out.put((i, func(x)))
+        q_out.put((i, func(x[0])))
 
 
 def pmap(
@@ -41,7 +41,7 @@ def pmap(
     Different from the python pool map, this function will not hang if any of
     the processes throws an exception.
     """
-    q_in: "Queue[Tuple[int, Optional[Inputs]]]" = Queue(1)
+    q_in: "Queue[Tuple[int, Optional[Tuple[Inputs]]]]" = Queue(1)
     q_out: "Queue[Tuple[int, Return]]" = Queue()
 
     proc = [
@@ -53,7 +53,7 @@ def pmap(
 
     count = 0
     for i, x in enumerate(inputs):
-        q_in.put((i, x))
+        q_in.put((i, (x,)))
         count += 1
     for _ in range(nprocs):
         q_in.put((-1, None))
