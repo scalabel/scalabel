e90105581c3470730001b7532f8c0ecd182f589c
Fisher Yu <fyu@users.noreply.github.com>
2021-06-07 22:09:02 +0200

    Add mac python test in ci (#281)
    * Add mac python test in ci
    
    * reset threshold in mask_to_polygon from 0.5 to 1.0
    
    * make tolerance a constant
    
    * remove extra space
    
    Co-authored-by: XiaLiPKU <ethanlee@pku.edu.cn>

--- scalabel/label/transforms.py
+++ scalabel/label/transforms.py
@@ -21,6 +21,8 @@ __all__ = [
     "polygon_to_poly2ds",
 ]
 
+TOLERANCE = 1.0
+
 
 def get_coco_categories(config: Config) -> List[CatType]:
     """Get CatType categories for saving these in COCO format annotations."""
