bd4a929ff63abd9b113386ba3c03dfd1537e9ecf
tobiasfshr <tobias.fischer@vision.ee.ethz.ch>
2021-06-25 22:15:52 +0200

    update waymo test

--- scalabel/label/from_waymo_test.py
+++ scalabel/label/from_waymo_test.py
@@ -2,9 +2,59 @@
 import os
 import shutil
 from argparse import Namespace
+from typing import List
 
 from ..unittest.util import get_test_file
 from .from_waymo import run
+from .io import load
+from .typing import Frame
+
+
+def compare_results(result: List[Frame], result_compare: List[Frame]) -> None:
+    """Compare two list of frames."""
+    for frame, frame_ref in zip(result, result_compare):
+        assert frame.name == frame_ref.name
+        assert frame.video_name == frame_ref.video_name
+        assert frame.frame_index == frame_ref.frame_index
+        if frame.intrinsics is not None:
+            assert frame_ref.intrinsics is not None
+            assert frame.intrinsics.focal == frame_ref.intrinsics.focal
+            assert frame.intrinsics.center == frame_ref.intrinsics.center
+            assert frame.intrinsics.skew == frame_ref.intrinsics.skew
+        else:
+            assert frame.intrinsics == frame_ref.intrinsics
+        if frame.extrinsics is not None:
+            assert frame_ref.extrinsics is not None
+            assert frame.extrinsics.location == frame_ref.extrinsics.location
+            assert frame.extrinsics.rotation == frame_ref.extrinsics.rotation
+        else:
+            assert frame.extrinsics == frame_ref.extrinsics
+
+        if frame.labels is not None:
+            assert frame_ref.labels is not None
+            for label, label_ref in zip(frame.labels, frame_ref.labels):
+                assert label.id == label_ref.id
+                assert label.category == label_ref.category
+                if label.box2d is not None:
+                    assert label_ref.box2d is not None
+                    assert label.box2d.x1 == label_ref.box2d.x1
+                    assert label.box2d.y1 == label_ref.box2d.y1
+                    assert label.box2d.x2 == label_ref.box2d.x2
+                    assert label.box2d.y2 == label_ref.box2d.y2
+                else:
+                    assert label.box2d == label_ref.box2d
+
+                if label.box3d is not None:
+                    assert label_ref.box3d is not None
+                    assert label.box3d.location == label_ref.box3d.location
+                    assert label.box3d.dimension == label_ref.box3d.dimension
+                    assert (
+                        label.box3d.orientation == label_ref.box3d.orientation
+                    )
+                else:
+                    assert label.box3d == label_ref.box3d
+        else:
+            assert frame.labels == frame_ref.labels
 
 
 def test_run() -> None:
@@ -21,16 +71,15 @@ def test_run() -> None:
         nproc=4,
     )
     run(args)
-    result = open(os.path.join(output_dir, "scalabel_anns.json"), "r").read()
-    result_file = open(result_filepath, "r").read()
-    assert result == result_file
-
+    result = load(os.path.join(output_dir, "scalabel_anns.json")).frames
+    result_compare = load(result_filepath).frames
+    compare_results(result, result_compare)
     os.remove(os.path.join(output_dir, "scalabel_anns.json"))
     args.use_lidar_labels = True
     run(args)
-    result = open(os.path.join(output_dir, "scalabel_anns.json"), "r").read()
-    result_file = open(result_lidar_filepath, "r").read()
-    assert result == result_file
+    result = load(os.path.join(output_dir, "scalabel_anns.json")).frames
+    result_compare = load(result_lidar_filepath).frames
+    compare_results(result, result_compare)
 
     paths = [os.path.join(output_dir, cam) for cam in os.listdir(output_dir)]
     for path in paths:
