9ea418c53648f0b76261db4d683443022e015979
Fisher Yu <i@yf.io>
2021-07-08 23:53:41 +0200

    launch local redis server when necessary

--- app/test/setup/global_setup.ts
+++ app/test/setup/global_setup.ts
@@ -1,32 +1,49 @@
-// import * as child from "child_process"
+import * as child from "child_process"
+import * as redis from "redis"
+import { promisify } from "util"
 
 import Logger from "../../src/server/logger"
+import { getTestConfig } from "../server/util/util"
+
+/**
+ * Launch redis server
+ */
+function launchRedisServer(): void {
+  const redisProc = child.spawn("redis-server", [
+    "--appendonly",
+    "no",
+    "--save",
+    "",
+    "--port",
+    String(getTestConfig().redis.port),
+    "--bind",
+    "127.0.0.1",
+    "--protected-mode",
+    "yes",
+    "--loglevel",
+    "warning"
+  ])
+  redisProc.stdout.on("data", (data) => {
+    process.stdout.write(data)
+  })
+
+  redisProc.stderr.on("data", (data) => {
+    process.stdout.write(data)
+  })
+}
 
 module.exports = async () => {
   Logger.info(
     "Info logger is muted for concise test status report. " +
       "The switch is in test/setup/local_setup.ts"
   )
-  // Make sure this matches the redis port in test_config.yml
-  // const redisProc = child.spawn("redis-server", [
-  //   "--appendonly",
-  //   "no",
-  //   "--save",
-  //   "",
-  //   "--port",
-  //   "6378",
-  //   "--bind",
-  //   "127.0.0.1",
-  //   "--protected-mode",
-  //   "yes",
-  //   "--loglevel",
-  //   "warning"
-  // ])
-  // redisProc.stdout.on("data", (data) => {
-  //   process.stdout.write(data)
-  // })
+  const client = redis.createClient(getTestConfig().redis.port)
 
-  // redisProc.stderr.on("data", (data) => {
-  //   process.stdout.write(data)
-  // })
+  client.send_command("ping", undefined, function (_err, res) {
+    if (res === undefined) {
+      Logger.info("Can't find redis server. Launch local redis server.")
+      launchRedisServer()
+    }
+  })
+  await promisify(client.quit).bind(client)()
 }
