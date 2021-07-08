ff142c1654ac35f6bc3fd22df0d8bc22d0f6a01f
XiaLiPKU <ethanlee@pku.edu.cn>
2021-07-08 14:29:32 -0700

    fix pylint bugs

--- scalabel/common/io.py
+++ scalabel/common/io.py
@@ -18,14 +18,13 @@ def load_config(filepath: str) -> DictStrAny:
     """
     ext = os.path.splitext(filepath)[1]
     if ext == ".yaml":
-        config_dict = yaml.load(
-            open(filepath, "r").read(),
-            Loader=yaml.Loader,
-        )
+        with open(filepath, "r") as fp:
+            config_dict = yaml.load(fp.read(), Loader=yaml.Loader)
     elif ext == ".toml":
         config_dict = toml.load(filepath)
     elif ext == ".json":
-        config_dict = json.load(open(filepath, "r"))
+        with open(filepath, "r") as fp:
+            config_dict = json.load(fp)
     else:
         raise NotImplementedError(f"Config extention {ext} not supported")
     assert isinstance(config_dict, dict)
