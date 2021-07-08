ef026aec44232076648f04e4984f9ff01627cbb0
Fisher Yu <i@yf.io>
2021-07-08 23:01:58 +0200

    disable jest redis-server for now

--- app/test/setup/global_setup.ts
+++ app/test/setup/global_setup.ts
@@ -1,4 +1,4 @@
-import * as child from "child_process"
+// import * as child from "child_process"
 
 import Logger from "../../src/server/logger"
 
@@ -8,25 +8,25 @@ module.exports = async () => {
       "The switch is in test/setup/local_setup.ts"
   )
   // Make sure this matches the redis port in test_config.yml
-  const redisProc = child.spawn("redis-server", [
-    "--appendonly",
-    "no",
-    "--save",
-    "",
-    "--port",
-    "6378",
-    "--bind",
-    "127.0.0.1",
-    "--protected-mode",
-    "yes",
-    "--loglevel",
-    "warning"
-  ])
-  redisProc.stdout.on("data", (data) => {
-    process.stdout.write(data)
-  })
+  // const redisProc = child.spawn("redis-server", [
+  //   "--appendonly",
+  //   "no",
+  //   "--save",
+  //   "",
+  //   "--port",
+  //   "6378",
+  //   "--bind",
+  //   "127.0.0.1",
+  //   "--protected-mode",
+  //   "yes",
+  //   "--loglevel",
+  //   "warning"
+  // ])
+  // redisProc.stdout.on("data", (data) => {
+  //   process.stdout.write(data)
+  // })
 
-  redisProc.stderr.on("data", (data) => {
-    process.stdout.write(data)
-  })
+  // redisProc.stderr.on("data", (data) => {
+  //   process.stdout.write(data)
+  // })
 }
