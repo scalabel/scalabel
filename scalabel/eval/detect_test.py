95cd8d12687d8c188424a14526913c0ab51ed53c
Xia Li 李夏 <ethanlee@pku.edu.cn>
2021-08-17 19:42:21 +0200

    Unify eval formats (#334)
    * parallize det eval; unify format of eval_results
    
    * add break lines
    
    * Re-design EvalResult as a subclass of pydantic.BaseModel
    
    * fix CI issues
    
    * change init for COCOevalV2
    
    * fix a params wrong order bug
    
    * polish details
    
    * multiple decimal number with 100
    
    * remove formatting parameters from Result's API
    
    * add docstring for Result
    
    * purify API of BaseResult
    
    * fix lgtm alerts
    
    * refactor the API with a more clear design
    
    * '_' -> '/'; BaseResult -> Result; frame() -> pd_frame(); revert file names
    
    * remove parame 'row_breaks'; rename 'render_results' into generate_results
    
    * fix a typo
    
    * refact data fields as type: List[Dict, Union[int, float]]
    
    * fix linting issues
    
    * add __eq__ to fix lgtm issues
    
    * fix __eq__ linting issues
    
    * resolve conversations in reviewing
    
    * remove print functions from eval
    
    * remove unused imports

--- scalabel/eval/detect_test.py
+++ scalabel/eval/detect_test.py
@@ -2,6 +2,8 @@
 import os
 import unittest
 
+import numpy as np
+
 from ..label.io import load, load_label_config
 from ..unittest.util import get_test_file
 from .detect import evaluate_det
@@ -10,28 +12,105 @@ from .detect import evaluate_det
 class TestBDD100KDetectEval(unittest.TestCase):
     """Test cases for BDD100K detection evaluation."""
 
-    def test_det(self) -> None:
-        """Check detection evaluation correctness."""
-        cur_dir = os.path.dirname(os.path.abspath(__file__))
-        gts_path = "{}/testcases/track_sample_anns.json".format(cur_dir)
-        preds_path = "{}/testcases/bbox_predictions.json".format(cur_dir)
-        gts = load(gts_path).frames
-        preds = load(preds_path).frames
-        config = load_label_config(get_test_file("configs.toml"))
-        result = evaluate_det(gts, preds, config)
+    cur_dir = os.path.dirname(os.path.abspath(__file__))
+    gts_path = "{}/testcases/track_sample_anns.json".format(cur_dir)
+    preds_path = "{}/testcases/bbox_predictions.json".format(cur_dir)
+    gts = load(gts_path).frames
+    preds = load(preds_path).frames
+    config = load_label_config(get_test_file("det_configs.toml"))
+    result = evaluate_det(gts, preds, config, nproc=1)
+
+    def test_frame(self) -> None:
+        """Test case for the function frame()."""
+        data_frame = self.result.pd_frame()
+        categories = set(
+            [
+                "pedestrian",
+                "rider",
+                "car",
+                "truck",
+                "bus",
+                "train",
+                "motorcycle",
+                "bicycle",
+                "traffic light",
+                "traffic sign",
+                "OVERALL",
+            ]
+        )
+        self.assertSetEqual(categories, set(data_frame.index.values))
+
+        data_arr = data_frame.to_numpy()
+        APs = np.array(  # pylint: disable=invalid-name
+            [
+                41.37670388,
+                41.70985554,
+                66.14185519,
+                50.07078219,
+                0.30253025,
+                -1.0,
+                4.57462895,
+                -1.0,
+                -1.0,
+                -1.0,
+                34.02939267,
+            ]
+        )
+        self.assertTrue(
+            np.isclose(np.nan_to_num(data_arr[:, 0], nan=-1.0), APs).all()
+        )
+
+        overall_scores = np.array(
+            [
+                34.02939267,
+                55.32390041,
+                34.9387336,
+                20.91624814,
+                48.43646525,
+                64.28530467,
+                23.8773384,
+                38.05003678,
+                40.9516045,
+                25.55304933,
+                58.38594872,
+                66.04261954,
+            ]
+        )
+        self.assertTrue(
+            np.isclose(
+                np.nan_to_num(data_arr[-1], nan=-1.0), overall_scores
+            ).all()
+        )
+
+    def test_summary(self) -> None:
+        """Check evaluation scores' correctness."""
+        summary = self.result.summary()
         overall_reference = {
-            "AP": 0.3402939266840102,
-            "AP_50": 0.553239004090397,
-            "AP_75": 0.34938733599978766,
-            "AP_small": 0.2091624813537171,
-            "AP_medium": 0.48436465249988514,
-            "AP_large": 0.6428530466767323,
-            "AR_max_1": 0.23877338403079265,
-            "AR_max_10": 0.3805003678186741,
-            "AR_max_100": 0.40951604502247774,
-            "AR_small": 0.2555304932799571,
-            "AR_medium": 0.5838594871794872,
-            "AR_large": 0.6604261954261954,
+            "AP/pedestrian": 41.37670388442971,
+            "AP/rider": 41.70985553789942,
+            "AP/car": 66.14185518719762,
+            "AP/truck": 50.07078219289907,
+            "AP/bus": 0.3025302530253025,
+            "AP/train": -1.0,
+            "AP/motorcycle": 4.574628954954978,
+            "AP/bicycle": -1.0,
+            "AP/traffic light": -1.0,
+            "AP/traffic sign": -1.0,
+            "AP": 34.029392668401016,
+            "AP50": 55.323900409039695,
+            "AP75": 34.93873359997877,
+            "APs": 20.91624813537171,
+            "APm": 48.4364652499885,
+            "APl": 64.28530466767323,
+            "AR1": 23.877338403079264,
+            "AR10": 38.050036781867405,
+            "AR100": 40.95160450224777,
+            "ARs": 25.5530493279957,
+            "ARm": 58.38594871794871,
+            "ARl": 66.04261954261955,
         }
-        for key, val in overall_reference.items():
-            self.assertAlmostEqual(val, result[key])
+        self.assertSetEqual(set(summary.keys()), set(overall_reference.keys()))
+        for name, score in summary.items():
+            if np.isnan(score):
+                score = np.nan_to_num(score, nan=-1.0)
+            self.assertAlmostEqual(score, overall_reference[name])
