f9444390e17635d41357e780ea8a1191567a83b3
Tobias Fischer <36965290+tobiasfshr@users.noreply.github.com>
2021-08-31 12:45:52 +0200

    fix queue type in vis, fix parse function when not validating (#346)
    * fix queue type in vis, fix parse function when not validating
    
    * revert from Category() to None in from_coco
    
    Co-authored-by: Fisher Yu <fyu@users.noreply.github.com>

--- scalabel/label/from_coco.py
+++ scalabel/label/from_coco.py
@@ -49,7 +49,9 @@ def coco_to_scalabel(coco: GtType) -> Tuple[List[Frame], Config]:
         }
     img_id2img: Dict[int, ImgType] = {img["id"]: img for img in coco["images"]}
 
-    cats = [Category() for _ in range(len(coco["categories"]))]
+    cats = [
+        None for _ in range(len(coco["categories"]))
+    ]  # type: List[Optional[Category]]
     cat_id2name = {}
     for category in coco["categories"]:
         assert 0 < int(category["id"]) <= len(coco["categories"])
