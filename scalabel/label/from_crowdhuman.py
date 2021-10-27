ec74b250bf640b88dfdb6457f7643bda1b34af5c
tobiasfshr <tobias.fischer@vision.ee.ethz.ch>
2021-10-27 14:02:40 +0200

    add crowdhuman conversion, update from_mot, update doc

---
+++ scalabel/label/from_crowdhuman.py
@@ -0,0 +1,76 @@
+"""Convert CrowdHuman format dataset to Scalabel."""
+import argparse
+import json
+import os
+from typing import List
+
+from ..common.typing import DictStrAny
+from .io import save
+from .typing import Box2D, Category, Config, Dataset, Frame, Label
+
+
+def parse_arguments() -> argparse.Namespace:
+    """Parse the arguments."""
+    parser = argparse.ArgumentParser(description="crowdhuman to scalabel")
+    parser.add_argument(
+        "--input",
+        "-i",
+        help="path to Crowdhuman annotation file.",
+    )
+    parser.add_argument(
+        "--output",
+        "-o",
+        default=".'",
+        help="Output filename for Scalabel format annotations.",
+    )
+    return parser.parse_args()
+
+
+def parse_annotations(annotations: List[DictStrAny]) -> List[Label]:
+    """Parse annotations per frame."""
+    labels = []
+    for anno in annotations:
+        if not anno["tag"] == "person":
+            continue
+        if anno["extra"].get("ignore", 0) == 1:
+            continue
+
+        x, y, w, h = anno["fbox"]
+        box2d = Box2D(x1=x, y1=y, x2=x + w, y2=y + h)
+        label = Label(
+            id=anno["extra"]["box_id"],
+            category="pedestrian",
+            box2d=box2d,
+        )
+        labels.append(label)
+    return labels
+
+
+def from_crowdhuman(input_path: str, image_path: str = "./Images/") -> Dataset:
+    """Function converting CrowdHuman annotations to Scalabel format."""
+    frames = []
+    with open(input_path, "r", encoding="utf-8") as anno_file:
+        lines = anno_file.readlines()
+        for line in lines:
+            frame_json = json.loads(line)
+            name = frame_json["ID"]
+            frame = Frame(
+                name=name,
+                url=os.path.join(image_path, name + ".jpg"),
+                labels=parse_annotations(frame_json["gtboxes"]),
+            )
+            frames.append(frame)
+    dataset = Dataset(
+        frames=frames, config=Config(categories=[Category(name="pedestrian")])
+    )
+    return dataset
+
+
+def run(args: argparse.Namespace) -> None:
+    """Run conversion with command line arguments."""
+    result = from_crowdhuman(args.input)
+    save(args.output, result)
+
+
+if __name__ == "__main__":
+    run(parse_arguments())
--- scalabel/label/from_mot.py
+++ scalabel/label/from_mot.py
@@ -2,14 +2,12 @@
 import argparse
 import os
 from collections import defaultdict
-from typing import Dict, List, Union
-
-from PIL import Image
+from typing import Dict, List, Tuple, Union
 
 from ..common.io import load_file_as_list
 from .io import save
 from .transforms import bbox_to_box2d
-from .typing import Frame, ImageSize, Label
+from .typing import Category, Config, Dataset, Frame, Label
 
 # Classes in MOT:
 #   1: 'pedestrian'
@@ -56,6 +54,11 @@ def parse_arguments() -> argparse.Namespace:
         default=".'",
         help="Output path for Scalabel format annotations.",
     )
+    parser.add_argument(
+        "--split-val",
+        action="store_true",
+        help="Split each video into train and validation parts (50 / 50).",
+    )
     return parser.parse_args()
 
 
@@ -88,10 +91,15 @@ def parse_annotations(ann_filepath: str) -> Dict[int, List[Label]]:
     return outputs
 
 
-def from_mot(data_path: str) -> List[Frame]:
+def from_mot(
+    data_path: str, split_val: bool = False
+) -> Union[Dataset, Tuple[Dataset, Dataset]]:
     """Function converting MOT annotations to Scalabel format."""
-    frames = []
+    frames, val_frames = [], []
     for video in sorted(os.listdir(data_path)):
+        if not os.path.isdir(os.path.join(data_path, video)):
+            continue
+        video_frames = []
         img_names = sorted(os.listdir(os.path.join(data_path, video, "img1")))
         annotations = parse_annotations(
             os.path.join(data_path, video, "gt/gt.txt"),
@@ -100,23 +108,40 @@ def from_mot(data_path: str) -> List[Frame]:
         for i, img_name in enumerate(img_names):
             assert i + 1 == int(img_name.replace(".jpg", ""))
             relative_path = os.path.join(video, "img1", img_name)
-            img = Image.open(os.path.join(data_path, relative_path))
             frame = Frame(
                 name=img_name,
-                video_name=video,
+                videoName=video,
                 url=relative_path,
-                frame_index=i,
-                size=ImageSize(width=img.width, height=img.height),
+                frameIndex=i,
                 labels=annotations[i + 1] if i in annotations else None,
             )
-            frames.append(frame)
-    return frames
+            video_frames.append(frame)
+        if split_val:
+            split_frame = len(img_names) // 2 + 1
+            frames.extend(video_frames[:split_frame])
+            for val_frame in video_frames[split_frame:]:
+                assert val_frame.frameIndex is not None
+                val_frame.frameIndex -= split_frame
+            val_frames.extend(video_frames[split_frame:])
+        else:
+            frames.extend(video_frames)
+
+    cfg = Config(categories=[Category(name="pedestrian")])
+    dataset = Dataset(frames=frames, config=cfg)
+    if split_val:
+        return dataset, Dataset(frames=val_frames, config=cfg)
+    return dataset
 
 
 def run(args: argparse.Namespace) -> None:
     """Run conversion with command line arguments."""
-    result = from_mot(args.input)
-    save(os.path.join(args.output, "scalabel_anns.json"), result)
+    result = from_mot(args.input, args.split_val)
+    if isinstance(result, tuple):
+        train, val = result
+        save(os.path.join(args.output, "train_scalabel.json"), train)
+        save(os.path.join(args.output, "val_scalabel.json"), val)
+    else:
+        save(os.path.join(args.output, "annotations_scalabel.json"), result)
 
 
 if __name__ == "__main__":
