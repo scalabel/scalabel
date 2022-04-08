67f637a103c93588300e4d1b3491e6fa11c410d7
cwlroda <weiloon.c97@gmail.com>
2022-04-08 03:52:52 +0200

    Fixed bugs

--- scripts/setup_ubuntu.sh
+++ scripts/setup_ubuntu.sh
@@ -2,6 +2,7 @@
 
 ROOT=0
 DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)
+PARENT_DIR=$(dirname ${DIR})
 LINE=$(printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' |tr ' ' '-')
 
 # Display underlined title to improve readability of script output
@@ -37,65 +38,105 @@ while getopts ":h" option; do
     esac
 done
 
-if [[ $ROOT -eq 0 ]]; then
+if [[ $ROOT -eq 0 ]]
+then
+    echo ""
     echo [$(date +"%F %T")] ================================
     echo [$(date +"%F %T")] RUNNING AS USER $(whoami)
     echo [$(date +"%F %T")] ================================
+    echo ""
 
-    if [[ ! hash nvm 2>/dev/null ]]; then
+    if ! command -v nvm >/dev/null
+    then
         echo [$(date +"%F %T")] ================================
         echo [$(date +"%F %T")] Installing NVM
         echo [$(date +"%F %T")] ================================
+        echo ""
 
         curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
-        nvm --version
+
+        export NVM_DIR="$HOME/.nvm"
+        [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+        [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+    else
+        echo [$(date +"%F %T")] ================================
+        echo [$(date +"%F %T")] NVM already installed
+        echo [$(date +"%F %T")] ================================
     fi
 
-    nvm install stable && nvm use stable
+    NVM_VER=$(nvm --version)
+    echo ""
+    echo [$(date +"%F %T")] ================================
+    echo [$(date +"%F %T")] NVM version: ${NVM_VER}
+    echo [$(date +"%F %T")] ================================
+    echo ""
+    nvm install stable
 
-    if [[ ! hash redis-server 2>/dev/null ]]; then
+    if ! command -v redis-server >/dev/null
+    then
+        echo ""
         echo [$(date +"%F %T")] ================================
         echo [$(date +"%F %T")] Installing Redis
         echo [$(date +"%F %T")] ================================
+        echo ""
 
         wget https://download.redis.io/redis-stable.tar.gz
         tar -xzvf redis-stable.tar.gz
         cd redis-stable
         make -j$(nproc)
+        # make test -j$(nproc)
 
-        REDIS_VER=$(src/redis-server --version | awk '{print substr($3, 3)}')
-        echo "Installed redis version: ${REDIS_VER}"
         cd ..
         rm -rf redis-stable.tar.gz
 
-        REDIS_PATH="${DIR}/redis/redis-${REDIS_VER}/src/redis-server"
+        REDIS_PATH="${PARENT_DIR}/redis-stable/src/redis-server"
         shopt -s expand_aliases
         echo "alias redis-server=\"${REDIS_PATH}\"" >> ~/.bash_aliases
         source ~/.bash_aliases
+    else
+        echo ""
+        echo [$(date +"%F %T")] ================================
+        echo [$(date +"%F %T")] Redis already installed
+        echo [$(date +"%F %T")] ================================
     fi
 
-    if [[ ! hash conda 2>/dev/null ]]; then
+    REDIS_VER=$(redis-server --version | awk '{print substr($3, 3)}')
+    echo ""
+    echo [$(date +"%F %T")] ================================
+    echo [$(date +"%F %T")] Redis version: ${REDIS_VER}
+    echo [$(date +"%F %T")] ================================
+    echo ""
+
+    if ! command -v conda >/dev/null
+    then
         echo [$(date +"%F %T")] ================================
         echo [$(date +"%F %T")] Installing conda
         echo [$(date +"%F %T")] ================================
 
-        . ${DIR}/scripts/install_conda.sh
+        . ${DIR}/install_conda.sh ${PARENT_DIR}
+
+        eval "$(${PARENT_DIR}/conda/bin/conda shell.bash hook)"
+        echo '[[ -f ${PARENT_DIR}/conda/bin/conda ]] && eval "$(${PARENT_DIR}/conda/bin/conda shell.bash hook)"' >> ${HOME}/.bashrc
+        echo ""
     fi
 
     echo [$(date +"%F %T")] ================================
     echo [$(date +"%F %T")] Creating conda environment
     echo [$(date +"%F %T")] ================================
 
-    conda env create -f ${DIR}/scripts/env.yml
+    conda env create -f ${DIR}/env.yml
 
 else
+    echo ""
     echo [$(date +"%F %T")] ================================
     echo [$(date +"%F %T")] RUNNING AS ROOT
     echo [$(date +"%F %T")] ================================
+    echo ""
 
     echo [$(date +"%F %T")] ================================
     echo [$(date +"%F %T")] Installing redis, node and python3.8
     echo [$(date +"%F %T")] ================================
+    echo ""
 
     sudo apt-get update
     sudo apt-get install -y --no-install-recommends ca-certificates \
@@ -109,33 +150,44 @@ else
         nodejs redis-server python3.8 \
         python3.8-dev python3-pip python3-setuptools python3.8-venv
 
+    echo ""
     echo [$(date +"%F %T")] ================================
     echo [$(date +"%F %T")] Installing python dependencies
     echo [$(date +"%F %T")] ================================
+    echo ""
 
     sudo python3.8 -m pip install -U pip
-    python3.8 -m pip install --user -U --ignore-installed -r ${DIR}/scripts/requirements.txt
+    python3.8 -m pip install --user -U --ignore-installed -r ${DIR}/requirements.txt
 fi
 
+echo [$(date +"%F %T")] ================================
+echo [$(date +"%F %T")] Setting up local directories
+echo [$(date +"%F %T")] ================================
+echo ""
+
+. scripts/setup_local_dir.sh
+
+echo ""
 echo [$(date +"%F %T")] ================================
 echo [$(date +"%F %T")] Installing node packages
 echo [$(date +"%F %T")] ================================
 
 npm install --max_old_space_size=8000
 
+echo ""
 echo [$(date +"%F %T")] ================================
 echo [$(date +"%F %T")] Compiling source code
 echo [$(date +"%F %T")] ================================
+echo ""
 
 node --max_old_space_size=8000 node_modules/.bin/webpack --config webpack.config.js --mode=production
 
+echo ""
 echo [$(date +"%F %T")] ================================
 echo [$(date +"%F %T")] Post-installation actions
 echo [$(date +"%F %T")] ================================
 
 title "Set up packages"
 echo "python setup.py develop"
-title "Set up local directories"
-echo "scripts/setup_local_dir.sh"
 echo ""
 echo ""
