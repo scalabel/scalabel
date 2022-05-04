45c4c09968d6140503259793fb182e04165e0aab
siyliepfl <lisiyu@student.ethz.ch>
2022-05-04 16:38:18 +0200

    fix lint

--- scalabel/eval/mots.py
+++ scalabel/eval/mots.py
@@ -153,7 +153,11 @@ def evaluate_seg_track(
 
     Returns:
         TrackResult: evaluation results.
+
+    Raises:
+        ValueError: if test results have videos that not present in gts.
     """
+
     logger.info("Tracking evaluation with CLEAR MOT metrics.")
     t = time.time()
     # check the missing video sequences
@@ -164,19 +168,23 @@ def evaluate_seg_track(
             gt_video_name_index_mapping[gt[0].videoName] = i
 
         gt_video_names = set(gt_video_name_index_mapping.keys())
-        res_video_names = set([res[0].videoName for res in results])
+        res_video_names = {res[0].videoName for res in results}
         missing_video_names = gt_video_names - res_video_names
         outlier_results = res_video_names - gt_video_names
 
         if outlier_results:
             logger.critical(
-                f"You have videos not in the test set: {str(outlier_results)} !"
+                "You have videos not in the test set:"
+                "%s", str(outlier_results)
             )
-            raise ValueError
+            raise ValueError(f"You have videos not in the test set:"
+                              f"{str(outlier_results)}")
 
         if missing_video_names:
-            logger.critical(f"The results are missing for "
-                            f"following video sequences: {str(missing_video_names)} !")
+            logger.critical("The results are missing for "
+                         "following video sequences: "
+                         "%s",  str(missing_video_names)
+                         )
 
             # add empty list for those missing results
             for name in missing_video_names:
@@ -196,8 +204,9 @@ def evaluate_seg_track(
             )
     else:
         logger.critical(
-            f"You have video names that are not in the test set!"
+            "You have video names that are not in the test set!"
         )
+
     assert len(gts) == len(results)
     # check overlap of masks
     logger.info("checking for overlap of masks...")
