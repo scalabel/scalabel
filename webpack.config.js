64ed4955351e2140bce9c04bd2ba3ab914b00797
Fisher Yu <i@yf.io>
2022-05-06 23:06:50 +0200

    Remove react-split-pane due to its deprecated react support

--- webpack.config.js
+++ webpack.config.js
@@ -70,6 +70,16 @@ let config = {
         },
       },
       {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
       }
