7f3eea3a63664e41230bd6008c356efdc469b82c
Fisher Yu <i@yf.io>
2022-05-10 17:38:16 +0200

    remove the deprecated go support

--- scripts/install_go_packages.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-go get github.com/aws/aws-sdk-go github.com/mitchellh/mapstructure \
-    gopkg.in/yaml.v2 github.com/satori/go.uuid github.com/dgrijalva/jwt-go
-
-curl -sfL https://install.goreleaser.com/github.com/golangci/golangci-lint.sh \
-    | sh -s -- -b $(go env GOPATH)/bin v1.17.1
-
