fb8a544998e448d657f88ed33f242431fb3ebca5
Xu Han <hxhxhx88@gmail.com>
2022-05-30 09:25:39 +0800

    improved the unit test

--- app/test/server/middleware.test.ts
+++ app/test/server/middleware.test.ts
@@ -1,6 +1,7 @@
 import express from "express"
 import request from "supertest"
 import { parseMultipartFormData } from "../../src/server/middleware/multipart"
+import { File } from "formidable"
 
 describe("test middleware", () => {
   test("parsing multipart/form-data", (done) => {
@@ -11,8 +12,8 @@ describe("test middleware", () => {
       parseMultipartFormData(req)
         .then(({ fields, files }) => {
           expect(fields).toMatchObject({ foo: "bar", hello: "world" })
-          expect(files.file1).toHaveProperty("filepath")
-          expect(files.file2).toHaveProperty("filepath")
+          expectSingleFile(files.file1)
+          expectSingleFile(files.file2)
           res.sendStatus(200)
         })
         .catch(() => res.sendStatus(500))
@@ -29,3 +30,17 @@ describe("test middleware", () => {
       .catch(done)
   })
 })
+
+/**
+ * Expect that a single valid `File` is provided.
+ *
+ * @param f - the parsed file(s)
+ */
+function expectSingleFile(f: File | File[]): void {
+  expect(f).toBeInstanceOf(File)
+
+  // formidable once changed its public API from using `path` to `filepath`.
+  // See https://github.com/scalabel/scalabel/pull/449/files.
+  // To avoid unexpected breaks in the future, we check if `filepath` is valid.
+  expect((f as File).filepath).toBeTruthy()
+}
