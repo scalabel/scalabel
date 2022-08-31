5e8a64037d17bd568f94ad0c77a540725a273f0f
Xu Han <xu.han@vision.ee.ethz.ch>
2022-08-31 11:14:44 +0200

    change the min size of polygon to 1

--- app/src/drawable/2d/common.ts
+++ app/src/drawable/2d/common.ts
@@ -2,7 +2,7 @@
 export const DASH_LINE = [15, 5]
 
 /** the min size of polygon */
-export const MIN_SIZE = 500
+export const MIN_SIZE = 1
 
 /** the opacity of filling color in polygon */
 export const OPACITY = [0.3]
