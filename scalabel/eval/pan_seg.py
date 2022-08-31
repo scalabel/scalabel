2104f9894b2874be772dc0ba9abed5fd1b61c208
Fisher Yu <i@yf.io>
2022-08-31 15:42:30 +0200

    fix lint due to pydantic update

--- scalabel/eval/pan_seg.py
+++ scalabel/eval/pan_seg.py
@@ -76,7 +76,7 @@ class PanSegResult(Result):
         """Convert the pan_seg data into a flattened dict as the summary."""
         summary_dict: Dict[str, Union[int, float]] = {}
         for metric, scores_list in self.dict(
-            include=include, exclude=exclude  # type: ignore
+            include=include, exclude=exclude
         ).items():
             summary_dict[f"{metric}/{STUFF}"] = scores_list[1][STUFF]
             summary_dict[f"{metric}/{THING}"] = scores_list[1][THING]
